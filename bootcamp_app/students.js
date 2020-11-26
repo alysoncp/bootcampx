bc31e8515c3200548234234cf17de917498afcfd
alysoncp <alyson.c.pickett@gmail.com>
2020-11-26 00:51:59 +0000

    student.js and teachers.js updated to use parameterized queries

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -7,17 +7,22 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
-const cohortName = process.argv[2];
-const limit = process.argv[3];
+const queryString = `
+  SELECT students.id as student_id, students.name as name, cohorts.name as cohort
+  FROM students
+  JOIN cohorts ON cohorts.id = cohort_id
+  WHERE cohorts.name LIKE $1
+  LIMIT $2;
+  `;
 
-pool.query(`
-SELECT students.id as student_id, students.name as name, cohorts.name as cohort
-FROM students
-JOIN cohorts ON students.cohort_id = cohorts.id
-WHERE cohorts.name LIKE '${cohortName}%'
-LIMIT ${limit};
-`)
+const cohortName = process.argv[2];
+const limit = process.argv[3] || 5;
+
+const values = [`%${cohortName}%`, limit];
+
+pool.query(queryString, values)
 .then(res => {
   console.log(res.rows);
+  pool.end();
 })
 .catch(err => console.error('query error', err.stack));
--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -7,19 +7,24 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
-const cohortName = process.argv[2];
-const limit = process.argv[3];
-
-pool.query(`
+const queryString = `
 SELECT DISTINCT teachers.name AS teacher_name, cohorts.name AS cohort_name
 FROM teachers
 JOIN assistance_requests ON teachers.id = assistance_requests.teacher_id
 JOIN students ON assistance_requests.student_id = students.id
 JOIN cohorts ON students.cohort_id = cohorts.id
-WHERE cohorts.name='JUL02'
-ORDER BY teachers.name;
-`)
+WHERE cohorts.name LIKE $1
+ORDER BY teachers.name
+LIMIT $2;
+`;
+
+const cohortName = process.argv[2];
+const limit = process.argv[3];
+const values = [`%${cohortName}%`, limit];
+
+pool.query(queryString, values)
 .then(res => {
   console.log(res.rows);
+  pool.end();
 })
 .catch(err => console.error('query error', err.stack));
